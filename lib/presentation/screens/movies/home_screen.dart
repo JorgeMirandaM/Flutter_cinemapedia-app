e7f30939aa8fbdcb371f585eed2f98658490aa28
JorgeMirandaM <jorge100099@hotmail.com>
2023-09-26 17:43:19 -0600

    appbar design finished

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: _HomeView(),
     );
   }
@@ -34,14 +34,20 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie=nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+
+        CustomAppbar(),
+        Expanded(child: ListView.builder(
+          itemCount: nowPlayingMovies.length,
+          itemBuilder: (context, index) {
+            final movie = nowPlayingMovies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          },
+        ),)
+      ],
     );
   }
 }
