c7f5db08b27f131b23456a5e22fc3b28d246ccf9
JorgeMirandaM <jorge100099@hotmail.com>
2023-10-01 16:45:09 -0600

    search movie finished

--- lib/presentation/providers/movies/movies_slideshow_provider.dart
+++ lib/presentation/providers/movies/movies_slideshow_provider.dart
@@ -5,6 +5,8 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+//Provider de solo lectura -Provider-
 final moviesSlidesshowProvider = Provider<List<Movie>>((ref) {
 
   final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
