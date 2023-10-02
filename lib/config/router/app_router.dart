ea08a088fdfc47a0445a2508c19d05f7c68f9d87
JorgeMirandaM <jorge100099@hotmail.com>
2023-10-01 23:41:16 -0600

    Router  and custom navigation configuration to navigate in different views

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,11 +1,17 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
+final appRouter = GoRouter(initialLocation: '/home/0', routes: [
   GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+
+        final pageIndex = state.pathParameters['page'] ?? '0';
+        
+
+        return  HomeScreen(pageIndex: int.parse(pageIndex));
+      },
       routes: [
         GoRoute(
           path: 'movie/:id',
@@ -16,4 +22,6 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
           },
         ),
       ]),
+
+      GoRoute(path: '/', redirect: (context, state) => '/home/0',)
 ]);
---
+++ lib/presentation/views/views.dart
@@ -0,0 +1,5 @@
+
+
+export 'movies/favorites_view.dart';
+
+export 'movies/home_view.dart';
