8acf5e730e8afa63467e99d2ad97bbdf64246515
JorgeMirandaM <jorge100099@hotmail.com>
2023-10-03 20:45:36 -0600

    message if there are no favorite movies in favorite movies screen

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/movies/movie_masonry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -11,7 +12,6 @@ class FavoritesView extends ConsumerStatefulWidget {
 }
 
 class FavoritesViewState extends ConsumerState<FavoritesView> {
-
   bool isLastPage = false;
   bool isLoading = false;
 
@@ -20,33 +20,48 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
     super.initState();
 
     loadNextPage();
-
   }
 
   void loadNextPage() async {
-
-    if ( isLoading || isLastPage ) return;
+    if (isLoading || isLastPage) return;
     isLoading = true;
 
-    final movies = await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    final movies =
+        await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
     isLoading = false;
 
-    if ( movies.isEmpty ) {
+    if (movies.isEmpty) {
       isLastPage = true;
     }
-
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
 
-    final favoriteMovies= ref.watch(favoriteMoviesProvider).values.toList();
+    if (favoriteMovies.isEmpty) {
+
+      final colors = Theme.of(context).colorScheme;
+
+
+      return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.favorite_outline_sharp, size: 60,color: colors.primary,),
+              Text('Ohhh no', style: TextStyle(fontSize: 30, color: colors.primary),),
+              const Text('No tienes peliculas favoritas', style: TextStyle(fontSize: 20, color: Colors.black45),),
+              const SizedBox(height: 20,),
+              FilledButton.tonal(onPressed: () => context.go('/home/0'), child: const Text('Empieza a buscar'))
+            ]),
+      );
+    }
 
     return Scaffold(
-      
-      body:MovieMansonry(movies:favoriteMovies, loadNextPage: loadNextPage,)
-    );
+        body: MovieMansonry(
+      movies: favoriteMovies,
+      loadNextPage: loadNextPage,
+    ));
   }
 }
